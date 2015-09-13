6028481cc3a3b0cf063201d20417577a646a5e02
Jonathan  Muller <jonathan.muller14@imperial.ac.uk>
2015-09-13 16:13:46 +0100

    Added skeleton content for members page

--- members.php
+++ members.php
@@ -8,12 +8,90 @@
     <?php require_once('header.php'); ?>
     <div class="container">
 
-      <div class="row">
-        <div class="col-lg-4">
-          col-lg-4
+      <div class="row members-body">
+        <div class="col-lg-3 col-xs-3">
+          <div class="profile-pic">
+            <img class="img-circle profile-pic" src="images/steve-profile.jpg">
+          </div>
         </div>
-        <div class="col-lg-8">
-          col-lg-8
+        <div class="col-lg-9 col-xs-3">
+          <h3>Steve Spisto</h3>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing
+          elit. Aenean commodo ligula eget dolor. Aenean massa
+          <strong>strong</strong>. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus
+          mus. Donec quam felis, ultricies nec, pellentesque
+          eu, pretium quis, sem. Nulla consequat massa quis
+          enim. Donec pede justo, fringilla vel, aliquet nec,
+          vulputate eget, arcu. In enim justo, rhoncus ut,
+          imperdiet a, venenatis vitae, justo. Nullam dictum
+          felis eu pede <a class="external ext" href="#">link</a>
+          mollis pretium. Integer tincidunt. Cras dapibus.
+          Vivamus elementum semper nisi. Aenean vulputate
+          eleifend tellus. Aenean leo ligula, porttitor eu,
+          consequat vitae, eleifend ac, enim. Aliquam lorem ante,
+          dapibus in, viverra quis, feugiat a, tellus. Phasellus
+          viverra nulla ut metus varius laoreet. Quisque rutrum.
+          Aenean imperdiet. Etiam ultricies nisi vel augue.
+          Curabitur ullamcorper ultricies nisi.</p>
+        </div>
+      </div>
+
+      <div class="row members-body">
+        <div class="col-lg-3 col-xs-3">
+          <div class="profile-pic">
+            <img class="img-circle profile-pic" src="images/steve.jpg">
+          </div>
+        </div>
+        <div class="col-lg-9 col-xs-3">
+          <h3>Flora Scorer</h3>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing
+          elit. Aenean commodo ligula eget dolor. Aenean massa
+          <strong>strong</strong>. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus
+          mus. Donec quam felis, ultricies nec, pellentesque
+          eu, pretium quis, sem. Nulla consequat massa quis
+          enim. Donec pede justo, fringilla vel, aliquet nec,
+          vulputate eget, arcu. In enim justo, rhoncus ut,
+          imperdiet a, venenatis vitae, justo. Nullam dictum
+          felis eu pede <a class="external ext" href="#">link</a>
+          mollis pretium. Integer tincidunt. Cras dapibus.
+          Vivamus elementum semper nisi. Aenean vulputate
+          eleifend tellus. Aenean leo ligula, porttitor eu,
+          consequat vitae, eleifend ac, enim. Aliquam lorem ante,
+          dapibus in, viverra quis, feugiat a, tellus. Phasellus
+          viverra nulla ut metus varius laoreet. Quisque rutrum.
+          Aenean imperdiet. Etiam ultricies nisi vel augue.
+          Curabitur ullamcorper ultricies nisi.</p>
+        </div>
+      </div>
+
+      <div class="row members-body">
+        <div class="col-lg-3 col-xs-3">
+          <div class="profile-pic">
+            <img class="img-circle profile-pic" src="images/fox.png">
+          </div>
+        </div>
+        <div class="col-lg-9 col-xs-3">
+          <h3>Firstname Lastname</h3>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing
+          elit. Aenean commodo ligula eget dolor. Aenean massa
+          <strong>strong</strong>. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus
+          mus. Donec quam felis, ultricies nec, pellentesque
+          eu, pretium quis, sem. Nulla consequat massa quis
+          enim. Donec pede justo, fringilla vel, aliquet nec,
+          vulputate eget, arcu. In enim justo, rhoncus ut,
+          imperdiet a, venenatis vitae, justo. Nullam dictum
+          felis eu pede <a class="external ext" href="#">link</a>
+          mollis pretium. Integer tincidunt. Cras dapibus.
+          Vivamus elementum semper nisi. Aenean vulputate
+          eleifend tellus. Aenean leo ligula, porttitor eu,
+          consequat vitae, eleifend ac, enim. Aliquam lorem ante,
+          dapibus in, viverra quis, feugiat a, tellus. Phasellus
+          viverra nulla ut metus varius laoreet. Quisque rutrum.
+          Aenean imperdiet. Etiam ultricies nisi vel augue.
+          Curabitur ullamcorper ultricies nisi.</p>
         </div>
       </div>
 
