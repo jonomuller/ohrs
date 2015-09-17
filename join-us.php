7a2abe0d1cbc8265a1409e6f89fbb61401261f9a
Jonathan  Muller <jonomuller@gmail.com>
2015-09-17 20:37:48 +0100

    Added sidebar to join us page

--- join-us.php
+++ join-us.php
@@ -8,6 +8,25 @@
     <?php require_once('header.php'); ?>
     <div class="container">
 
+      <div class="row">
+        <div class="col-md-9">
+
+        </div>
+        <div class="col-md-3">
+          <div id="contact">
+            <h3>Contact us</h3>
+            <a href="mailto:flora.scorer@lmh.ox.ac.uk">flora.scorer@lmh.ox.ac.uk</a>
+            <a href="mailto:name@example.com">name@example.com</a>
+          </div>
+
+          <h3>Subscribe to our newsletter</h3>
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="email address" aria-describedby="basic-addon1">
+          </div>
+          <button type="button" class="btn btn-default">Subscribe</button>
+        </div>
+      </div>
+
     </div>
     <?php require_once('footer.php'); ?>
   </body>
